848cf8bf04c60fd3559477a59bd1261a990e5fbd
Myriam <salazarmyriarg@gmail.com>
2020-06-26 14:53:54 -0300

    se agrego el envío de mail

--- js/eventos.js
+++ js/eventos.js
@@ -1,23 +1,23 @@
-function campoRequerido(input){
-    if(input.value == ""){
-      //si el campo esta vacio
-      input.className = "form-control is-invalid";
-    }else{
+function campoRequerido(input) {
+    if (input.value == "") {
+        //si el campo esta vacio
+        input.className = "form-control is-invalid";
+    } else {
         //si esta completo
-      input.className = "form-control is-valid";
+        input.className = "form-control is-valid";
     }
 }
 
-function validarTelefono(input){
+function validarTelefono(input) {
     let quitarEspacio = / /;
-    if(input.value != "" && !isNaN(input.value) && !quitarEspacio.test(input.value)){
+    if (input.value != "" && !isNaN(input.value) && !quitarEspacio.test(input.value)) {
         input.className = "form-control is-valid";
-    }else{
+    } else {
         input.className = "form-control is-invalid";
     }
 }
 
-function validarMail(input){
+function validarMail(input) {
     //salazarmyriarg@gmail.com
     let expresion = /\w+@\w+\.[a-z]{2,}$/;
     if (input.value != "" && expresion.test(input.value)) {
@@ -27,20 +27,43 @@ function validarMail(input){
     }
 }
 
-function validarPais(texto){
-    if(texto.value == ""){
+function validarPais(texto) {
+    if (texto.value == "") {
         //si el campo esta vacio
         texto.className = "form-control is-invalid";
-      }else{
-          //si esta completo
+    } else {
+        //si esta completo
         texto.className = "form-control is-valid";
-      }
+    }
 }
 
-function validarConsulta(consult){
+function validarConsulta(consult) {
     if (consult.value != "" && consult.value.length >= 15) {
         consult.className = "form-control is-valid";
     } else {
         consult.className = "form-control is-invalid";
     }
+}
+
+function validarCompleto(event) {
+    event.preventDefault();
+    console.log("Dentro de Validar completo");
+
+    if (campoRequerido(document.getElementById("nombre")) && validarTelefono(document.getElementById("numero")) && validarMail(document.getElementById("email")) && validarPais(document.getElementById("pais")) && validarConsulta(document.getElementById("consulta"))
+    ) {
+        enviarMail();
+    } else {
+        alert("No pudimos enviar tu correo, hay un error");
+    }
+}
+
+function enviarMail() {
+    var template_params = {
+        "from_name": "from_name_value",
+        "message_html": `Mensaje: ${document.getElementById("consulta").value} - Email ${document.getElementById("email").value}`
+    }
+
+    var service_id = "default_service";
+    var template_id = "template_WsYqydVn";
+    emailjs.send(service_id, template_id, template_params);
 }
