965c207750f4984cd3efb456d1065eda01bd8399
Myriam <salazarmyriarg@gmail.com>
2020-06-26 15:53:38 -0300

    se agrega alert de envio

--- js/eventos.js
+++ js/eventos.js
@@ -2,9 +2,11 @@ function campoRequerido(input) {
     if (input.value == "") {
         //si el campo esta vacio
         input.className = "form-control is-invalid";
+        return false;
     } else {
         //si esta completo
         input.className = "form-control is-valid";
+        return true;
     }
 }
 
@@ -12,8 +14,10 @@ function validarTelefono(input) {
     let quitarEspacio = / /;
     if (input.value != "" && !isNaN(input.value) && !quitarEspacio.test(input.value)) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
@@ -22,8 +26,10 @@ function validarMail(input) {
     let expresion = /\w+@\w+\.[a-z]{2,}$/;
     if (input.value != "" && expresion.test(input.value)) {
         input.className = "form-control is-valid";
+        return true;
     } else {
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
@@ -31,17 +37,21 @@ function validarPais(texto) {
     if (texto.value == "") {
         //si el campo esta vacio
         texto.className = "form-control is-invalid";
+        return false;
     } else {
         //si esta completo
         texto.className = "form-control is-valid";
+        return true;
     }
 }
 
 function validarConsulta(consult) {
     if (consult.value != "" && consult.value.length >= 15) {
         consult.className = "form-control is-valid";
+        return true;
     } else {
         consult.className = "form-control is-invalid";
+        return false;
     }
 }
 
@@ -51,6 +61,7 @@ function validarCompleto(event) {
     if (campoRequerido(document.getElementById("nombre")) && validarTelefono(document.getElementById("numero")) && validarMail(document.getElementById("email")) && validarPais(document.getElementById("pais")) && validarConsulta(document.getElementById("consulta"))
     ) {
         enviarMail(this);
+        alert("Tu consulta se envió con éxito");
     } else {
         alert("No pudimos enviar tu correo, hay un error");
     }
